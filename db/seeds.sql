0fb2ed86727b2c83a14ae6ba3a1b508d2f5a2033
Mark <mark.faraj86@gmail.com>
2019-01-24 21:21:30 -0600

    initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Triple Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Burger', true);
