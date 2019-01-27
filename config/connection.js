3b07159abfa3542f1ecce916f2f79b60bd2126a8
Mark <mark.faraj86@gmail.com>
2019-01-26 20:02:21 -0600

    fixing heroku and Mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burger_db"
-});
-
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'burgers_db'
+	})
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //require express package
 var express = require("express");
-
+var bodyParser = require('body-parser');
 //create a new port to your connection.
 // left hand side for using a deployed website
 // right hand side for using local development
@@ -12,7 +12,7 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 // to retain the directory public as the main directory
-app.use(express.static("public"));
+app.use(bodyParser.static("public"));
 
 
 //This is a built-in middleware function in Express. 
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burger_controller.js");
 
-app.use(routes);
+app.use('/', routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
