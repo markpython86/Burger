79c003b1d7b57ed68c0e27af48a765ff40bbb146
Mark <mark.faraj86@gmail.com>
2019-01-26 22:12:19 -0600

    updating connection.js localhost to 127.0.0.1

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
 } else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
-		host: 'localhost',
+		host: '127.0.0.1',
 		port: 3306,
 		user: 'root',
 		password: 'root',
