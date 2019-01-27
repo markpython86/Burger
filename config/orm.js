f3fc7337059b0d9a5f47ff9b824d396d64c1c023
Mark <mark.faraj86@gmail.com>
2019-01-26 19:29:40 -0600

    finalizing the project

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,17 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
-
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
@@ -39,16 +31,16 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
+  all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  create: function(table, cols, vals, cb) {
+  create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -57,19 +49,16 @@ var orm = {
     queryString += "VALUES (";
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
-
     console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
-
       cb(result);
     });
   },
-  // An example of objColVals would be {name: Cheese Burger, devoured: true}
-  update: function(table, objColVals, condition, cb) {
+  //An example of objColVals would be {name: Cheese Burger, devoured: true}
+  update: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -78,7 +67,7 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
---
+++ server.js
@@ -0,0 +1,38 @@
+//require express package
+var express = require("express");
+
+//create a new port to your connection.
+// left hand side for using a deployed website
+// right hand side for using local development
+var PORT = process.env.PORT || 3000;
+
+
+//assign express application/package to a variable app
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+// to retain the directory public as the main directory
+app.use(express.static("public"));
+
+
+//This is a built-in middleware function in Express. 
+//It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.urlencoded({ extended: false }));
+// console.log("testing: "+testing)
+// Parse request body as JSON
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
